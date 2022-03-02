6d6adb8fb1355536942c0a104f5ddb7eff2877d3
mohitprakash <mohitprakash@localhost>
2022-03-02 14:19:16 +0530

    Commit1

---
+++ 17.SB_WebClient_App1/src/main/java/com/mps/Application.java
@@ -0,0 +1,18 @@
+package com.mps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.mps.service.TicketService;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		TicketService bean = context.getBean(TicketService.class);
+		bean.getTicket();
+	}
+
+}
---
+++ 17.SB_WebClient_App1/src/main/java/com/mps/service/TicketService.java
@@ -0,0 +1,36 @@
+package com.mps.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.Disposable;
+
+@Service
+public class TicketService {
+
+	public void getTicket()
+	{
+		String endpointUrl="https://irctc-railway-api.herokuapp.com/ticket/PNR3744";
+		
+		WebClient webClient=WebClient.create();
+		
+	/*	Disposable disposable = webClient.get() //want to send get request
+						.uri(endpointUrl) //which url to send get request
+						.retrieve()  //to retrieve the response after sending request
+						.bodyToMono(String.class)  //bind the response to String variable and Mono means we are getting single response from Rest API.
+						.subscribe();  //to make request asynchronous. block() is used to make request synchronous.
+		
+		boolean disposed = disposable.isDisposed();
+		
+		System.out.println("Disposed "+disposed);
+	*/
+		String block = webClient.get()
+						.uri(endpointUrl)
+						.retrieve()
+						.bodyToMono(String.class)
+						.block();
+		
+		System.out.println(block);
+	}
+	
+}
