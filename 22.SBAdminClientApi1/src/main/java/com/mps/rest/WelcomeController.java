ad8df265b29e872c605133380288277c7f3464ca
mohitprakash <mohitprakash@localhost>
2022-03-21 22:17:49 +0530

    Commit1

---
+++ 22.SBAdminClientApi1/src/main/java/com/mps/rest/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.mps.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public ResponseEntity<String> getMsg()
+	{
+		return new ResponseEntity<String>("Welcome.....",HttpStatus.OK);
+	}
+}
